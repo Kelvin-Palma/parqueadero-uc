1cf02243325f028449a528a36aafa99c6906dcc6
Kelvin Palma <kelvinpalmadev@gmail.com>
2026-09-18 20:32:14 -0500

    feat: Tarifa especial para visitantes los fines de semana

--- code.py
+++ code.py
@@ -14,6 +14,7 @@ while contador < cupos:
     placa = input("Placa: ").strip().upper()
     if placa == "SALIR":
         break
+
     tipo = input("Tipo de usuario (E=Estudiante, D=Docente, V=Visitante): ").strip().upper()
 
     entrada_hora = input("Hora de entrada (0 a 23): ")
@@ -40,33 +41,49 @@ while contador < cupos:
         print("Las horas de permanencia no pueden ser negativas.")
         continue
 
+    if tipo == "V":
+        dia = input("Día de la semana: ").strip().upper()
+    else:
+        dia = ""
+
     if tipo == "E":
         estudiantes += 1
         tarifa = 0 if permanencia <= 2 else (permanencia - 2) * 800
+
     elif tipo == "D":
         docentes += 1
         tarifa = permanencia * 500
+
     else:
         if tipo != "V":
             print("Tipo inválido. Se toma como visitante.")
+
         tipo = "V"
         visitantes += 1
+
         tarifa = 1500 if permanencia <= 1 else 1500 + (permanencia - 1) * 1200
 
+        if dia == "SABADO":
+            tarifa *= 0.80
+            print("Se aplicó el descuento especial del sábado: 20%.")
+
+    # No se aplica descuento si es sábado
     if hora > 19 or hora < 6:
-        tarifa *= 0.90
+        if not (tipo == "V" and dia == "SABADO"):
+            tarifa *= 0.90
+            print("Se aplicó el descuento nocturno del 10%.")
 
     recaudo += tarifa
     horas_totales += permanencia
     contador += 1
 
     print("Vehículo registrado correctamente.")
-    print("\nTarifa:", tarifa, "COP")
+    print("\nTarifa:", round(tarifa, 2), "COP")
 
     if contador == cupos:
         print("\nPARQUEADERO LLENO")
 
-print("\n==RESUMEN DEL DIA==")
+print("\n== RESUMEN DEL DIA ==")
 
 print("Vehículos registrados:", contador)
 print("Ocupación:", round((contador / 30) * 100, 2), "%")
@@ -82,6 +99,3 @@ else:
     promedio = 0
 
 print("Promedio de permanencia:", round(promedio, 2), "horas")
-
-
-
